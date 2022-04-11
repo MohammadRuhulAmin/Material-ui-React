d862b36187b73c50df4ad4713c6afe79636fd1dc
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2022-04-11 19:42:17 +0600

    Button & Typography

---
+++ src/Components/MuiButton.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {Stack,Button,IconButton} from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
+const MuiButton = () => {
+  return (
+      <div>
+        <Stack spacing={2} direction = 'row'>
+            <Button variant='text' href='https://google.com'>Texed</Button>
+            <Button variant = 'contained'>Contained</Button>
+            <Button variant='outlined'>Outlined</Button>
+        </Stack>
+        <Stack spacing={2} direction = 'row'>
+            <Button variant = 'contained' color='error'>Error</Button>
+            <Button variant = 'contained' color='success'>Success</Button>
+            <Button variant = 'contained' color='warning'>warning</Button>
+        </Stack>
+        <Stack spacing = {3} direction = 'row'>
+            <Button variant = 'outlined' color='primary'>Primary</Button>
+        </Stack>
+        <Stack spacing = {3} direction = 'row'>
+            <Button variant = 'contained' color = 'warning' size='small'>Small A</Button>
+            <Button variant = 'contained' color = 'success' size = 'medium'>Medium</Button>
+            <Button variant = 'contained' color = 'error'  size = 'large'>Large</Button>
+        </Stack>
+        <div>
+            <Button variant = 'contained' color = 'error' startIcon = {<SendIcon/>}> Send </Button>
+            <Button variant = 'contained' color = 'success' endIcon = {<SendIcon/>}>Send</Button>
+        </div>
+        <h1>Go to Live </h1>
+        <IconButton color = 'success' size = 'small'  onClick={()=>alert('Md RUhul Amin')}>
+            <SendIcon />
+        </IconButton>
+      </div>
+    
+  )
+}
+
+export default MuiButton
